e76d1da881d8da64e628d2bb84b257fc56d5e19d
Superrama <superrama86@gmail.com>
2026-04-04 12:58:50 +0100

    update

--- graph.py
+++ graph.py
@@ -1,29 +1,46 @@
+from dotenv import load_dotenv
+import os
+
+# Charge le .env
+load_dotenv(override=True)
+
+# Vérifie que la clé est bien chargée
+api_key = os.getenv("OPENAI_API_KEY")
+
+
 from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
-from dotenv.ipython import load_dotenv
+# from dotenv.ipython import load_dotenv
 from langchain.tools import tool
 from langchain.messages import SystemMessage, HumanMessage, AIMessage, ToolMessage
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.tools import create_retriever_tool
 from langchain_community.vectorstores import Chroma
+# from dotenv import load_dotenv
+# import os
 
-embedding_model = OpenAIEmbeddings(model="text-embedding-3-large")
+# embedding_model = OpenAIEmbeddings(model="text-embedding-3-large")
+embedding_model = OpenAIEmbeddings(
+    model="text-embedding-3-large",
+)
 
-load_dotenv(override=True)
+# load_dotenv(load_dotenv(override=True))
 
 
+
+# LLM
 llm = ChatOpenAI(model="gpt-4o", temperature=0)
 
 texts = [
     "Je suis une futur ai engineer , now I work in CIH bank",
-    "Je étudiant entrepreneur"
-    "Je suis passionné par le développement de l'intelligence artificielle et j'aime apprendre de nouvelles technologies"
+    "Je étudiant entrepreneur",
+    "Je suis passionné par le développement de l'intelligence artificielle et j'aime apprendre de nouvelles technologies",
     "Je travaille à CIH bank en tant que stagiaire dans le département de l'innovation, où je contribue à des projets liés à l'intelligence artificielle et à la transformation digitale"
     "Je gagne 5000 dirhams par mois et j'ai une seniorité de 1 an dans le domaine de l'intelligence artificielle"
 
 ]
 
-embedding_model = OpenAIEmbeddings(model = "test-embedding-ada-002") 
+# embedding_model = OpenAIEmbeddings(model = "test-embedding-ada-002") 
 vectorstore = Chroma.from_texts(texts, embedding_model,collection_name="CV_collection")
 
 retrieval = vectorstore.as_retriever(kwargs={"k":5})
@@ -38,7 +55,7 @@ def send_mail(mail:str, subject:str, content:str):
     print("send_mail tool invoked")
     print("=="*50)
 
-    return f"This mail has been sent : destination {email}, subject : {subject}, content : {content}" 
+    return f"This mail has been sent : destination {mail}, subject : {subject}, content : {content}" 
 
 @tool
 def get_employee_info (name: str):
@@ -49,9 +66,11 @@ def get_employee_info (name: str):
 
     return {"name": name, "salary": 15000, "seniority": 2}
 
+
+# Création de l'agent
 graph = create_agent(
     model=llm,
-    tools=[get_employee_info, retrieval_info, send_mail],
+    tools=[get_employee_info, retrieval_tool, send_mail],
     system_prompt="answer the user question using prived tools",
 
 )
