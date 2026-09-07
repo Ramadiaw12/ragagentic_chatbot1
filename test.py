800bd7d6a7dbd6a46959ed1bf530adbeb0eaac6a
Superrama <superrama86@gmail.com>
2026-09-07 12:44:15 +0100

    create an instance and an agent

--- test.py
+++ test.py
@@ -2,3 +2,25 @@ from langchain.agents import create_agent
 from langchain_groq import ChatGroq
 import os
 from dotenv.ipython import load_dotenv
+from IPython.display import Mardown
+
+# Load environnement variable from .env file
+load_dotenv(override=True)
+
+# Create a chatgroq instance avec specific model 
+llm = ChatGroq(
+    model= "openai/gpt-oss-120b",
+    temperature=0
+
+)
+
+# Create an agent with a chatgroq instance
+agent = create_agent(
+    model=llm,
+    system_prompt= "you are a helpful assistant"
+)
+
+# Invoke the agent from the user messages
+resp= agent.invoke(input={"messages":[{"role":"user", "context":"myname is Rahma"}]})
+# print the last messages content from the agent response
+print(resp["messages"][-1].content)
